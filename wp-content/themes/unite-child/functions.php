982cc6292b815a913b024a4cc176d6e728d3eaac
Mahesh Sambherao <kskn.mahesh@gmail.com>
2018-07-08 13:15:18 +0530

    Added support for enqueuing Unite parent theme CSS, installed plugin for advanced fields

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -5,6 +5,12 @@
 
 define( 'UNITE_CHILD_DIR', get_template_directory() . '-child' ); // TextDomain for the theme
 
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
 /**
  * Include Films Configurations.
  */
